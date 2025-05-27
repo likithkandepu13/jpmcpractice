c7e29bb07bbfbbe2a90e86d3eab3322ce286521f
likithkandepu13 <withlikith@gmail.com>
2025-05-27 19:50:28 +0530

    added functions practice

---
+++ javascript/functions.js
@@ -0,0 +1,28 @@
+// java script functions
+
+function funcName(){
+    // Function logic goes here
+    console.log("Function executed");
+}
+// basics syntax
+
+function add(a, b) {
+    // This function adds two numbers
+    return a + b;
+}
+add(5, 3); // Returns 8
+
+function subtract(a, b) {
+    // This function subtracts the second number from the first
+    return a - b;
+}
+// arrow functions  
+const multiply = (a, b) => {
+    // This function multiplies two numbers
+    return a * b;
+};
+multiply(4, 2); // Returns 8
+const divide = (a, b) => {
+    // This function divides the first number by the second
+    return a / b;
+};
